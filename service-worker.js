d88e029bbcba28f6b84771a5298bca027e96915c
findme21910-jimmy <findme21910@gmail.com>
2026-09-12 20:13:28 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = 'find-me-v1';
+const CACHE_NAME = 'find-me-v2';
 
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './icons/192.png',
+  './icons/512.png'
 ];
 
 self.addEventListener('install', event => {
